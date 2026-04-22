5375d2a58f31854a3a41bf2aa2968b38d6aadaa8
Iris Yan <iy8488@princeton.edu>
2026-04-21 23:55:57 -0400

    wrote most of training loop

--- model.py
+++ model.py
@@ -4,13 +4,15 @@ from jaxtyping import Float, Array, Bool, Int
 import jax.numpy as jnp
 import jax
 from einops import rearrange, einsum
+from typing import Optional
 
 
 class Attention(nnx.Module):
     def __init__(self, num_heads: int, hidden_size: int, rngs: nnx.Rngs):
-        self.num_heads = num_heads
-        self.hidden_size = hidden_size
-        self.head_dim = self.hidden_size // self.num_heads
+        self.num_heads: int = num_heads
+        self.hidden_size: int = hidden_size
+        self.head_dim: int = self.hidden_size // self.num_heads
+        self.Wq = nnx.Linear(hidden_size, hidden_size, rngs=rngs)
         self.Wq = nnx.Linear(hidden_size, hidden_size, rngs=rngs)
         self.Wk = nnx.Linear(hidden_size, hidden_size, rngs=rngs)
         self.Wv = nnx.Linear(hidden_size, hidden_size, rngs=rngs)
@@ -18,33 +20,32 @@ class Attention(nnx.Module):
 
     def __call__(
         self,
-        query: Float[Array, "B Lq D"],
-        key: Float[Array, "B Lk D"],
-        value: Float[Array, "B Lk D"],
-        mask: Bool[Array, "B 1 Lq Lk"],
-    ) -> Float[Array, "B Lq D"]:
-        B, Lq, D = query.shape
-        _, Lk, _ = key.shape
+        query: Float[Array, "B L D"],
+        key: Float[Array, "B L D"],
+        value: Float[Array, "B L D"],
+        mask: Bool[Array, "B 1 L L"],
+    ) -> Float[Array, "B L D"]:
+        B, L, D = query.shape
 
-        Q: Float[Array, "B H Lq d_k"] = rearrange(
-            self.Wq(query), "B Lq (H d_k) -> B H Lq d_k"
+        Q: Float[Array, "B H L d_k"] = rearrange(
+            self.Wq(query), "B L (H d_k) -> B H L d_k"
         )
-        K: Float[Array, "B H Lk d_k"] = rearrange(
-            self.Wk(key), "B Lk (H d_k) -> B H Lk d_k"
+        K: Float[Array, "B H L d_k"] = rearrange(
+            self.Wk(key), "B L (H d_k) -> B H L d_k"
         )
-        V: Float[Array, "B H Lk d_k"] = rearrange(
-            self.Wv(value), "B Lk (H d_k) -> B H Lk d_k"
+        V: Float[Array, "B H L d_k"] = rearrange(
+            self.Wv(value), "B L (H d_k) -> B H L d_k"
         )
 
-        scores: Float[Array, "B H Lq Lk"] = einsum(
-            Q, K, "B H Lq d_k, B H Lk d_k -> B H Lq Lk"
+        scores: Float[Array, "B H L L"] = einsum(
+            Q, K, "B H L d_k, B H L d_k -> B H L L"
         ) / jnp.sqrt(self.head_dim)
-        softmax: Float[Array, "B H Lq Lk"] = jax.nn.softmax(scores, axis=-1, where=mask)
-        attention: Float[Array, "B H Lq d_k"] = einsum(
-            softmax, V, "B H Lq Lk, B H Lk d_k -> B H Lq d_k"
+        softmax: Float[Array, "B H L L"] = jax.nn.softmax(scores, axis=-1, where=mask)
+        attention: Float[Array, "B H L d_k"] = einsum(
+            softmax, V, "B H L L, B H L d_k -> B H L d_k"
         )
-        multi_attention: Float[Array, "B Lq D"] = rearrange(
-            attention, "B H Lq d_k -> B Lq (H d_k)"
+        multi_attention: Float[Array, "B L D"] = rearrange(
+            attention, "B H L d_k -> B L (H d_k)"
         )
         return self.Wo(multi_attention)
 
@@ -61,16 +62,16 @@ class Embedding(nnx.Module):
         self.Embedding: nnx.Embed = nnx.Embed(vocab_size, hidden_size, rngs=rngs)
 
         den = jnp.exp(-jnp.arange(0, hidden_size, 2) * jnp.log(10000.0) / hidden_size)
-        pos: Int[Array, "Lq 1"] = jnp.arange(0, seq_length)[:, jnp.newaxis]
-        pe: Float[Array, "Lq D"] = jnp.zeros((seq_length, hidden_size))
+        pos: Int[Array, "L 1"] = jnp.arange(0, seq_length)[:, jnp.newaxis]
+        pe: Float[Array, "L D"] = jnp.zeros((seq_length, hidden_size))
         pe = pe.at[:, 0::2].set(jnp.sin(pos * den))
         pe = pe.at[:, 1::2].set(jnp.cos(pos * den))
         self.pos_embedding = nnx.Variable(pe)
         self.dropout = nnx.Dropout(dropout_prob, rngs=rngs)
 
-    def __call__(self, tokens: Int[Array, "B Lq"]) -> Float[Array, "B Lq D"]:
-        curr_seqlength = tokens.shape[1]
-        token_embedding = self.Embedding(tokens)
+    def __call__(self, tokens: Int[Array, "B L"]) -> Float[Array, "B L D"]:
+        curr_seqlength: int = tokens.shape[1]
+        token_embedding: Float[Array, "B L D"] = self.Embedding(tokens)
         return self.dropout(
             token_embedding + self.pos_embedding.value[:curr_seqlength, :]
         )
@@ -99,18 +100,19 @@ class Transformer(nnx.Module):
 
     def __call__(
         self,
-        x: Float[Array, "B Lq D"],
-        encoder_output: Float[Array, "B Lk D"],
-        self_mask: Bool[Array, "B 1 Lq Lk"],
-        cross_mask: Bool[Array, "B 1 Lq Lk"],
-    ) -> Float[Array, "B Lq D"]:
+        x: Float[Array, "B L D"],
+        encoder_output: Optional[Float[Array, "B L D"]],
+        self_mask: Bool[Array, "B 1 L L"],
+        cross_mask: Optional[Bool[Array, "B 1 L L"]],
+    ) -> Float[Array, "B L D"]:
         norm: Float[Array, "B L D"] = self.norm1(x)
         x += self.self_attention(norm, norm, norm, self_mask)
 
-        norm: Float[Array, "B Lq D"] = self.norm2(x)
-        x += self.cross_attention(norm, encoder_output, encoder_output, cross_mask)
+        if encoder_output is not None and cross_mask is not None:
+            norm: Float[Array, "B L D"] = self.norm2(x)
+            x += self.cross_attention(norm, encoder_output, encoder_output, cross_mask)
 
-        norm: Float[Array, "B Lq D"] = self.norm3(x)
+        norm: Float[Array, "B L D"] = self.norm3(x)
         x += self.ffn(norm)
         return x
 
@@ -140,14 +142,14 @@ class DecoderModel(nnx.Module):
 
     def __call__(
         self,
-        tokens: Int[Array, "B Lq V"],
-        encoder_output: Float[Array, "B Lk D"],
-        self_mask: Bool[Array, "B 1 Lq Lk"],
-        cross_mask: Bool[Array, "B 1 Lq Lk"],
+        tokens: Int[Array, "B L"],
+        encoder_output: Optional[Float[Array, "B L D"]],
+        self_mask: Bool[Array, "B 1 L L"],
+        cross_mask: Optional[Bool[Array, "B 1 L L"]],
     ):
-        hidden_output: Float[Array, "B Lq D"] = self.embedding(tokens)
+        hidden_output: Float[Array, "B L D"] = self.embedding(tokens)
         for layer in self.decoder:
-            hidden_output: Float[Array, "B Lq D"] = layer(
+            hidden_output: Float[Array, "B L D"] = layer(
                 hidden_output, encoder_output, self_mask, cross_mask
             )
         return self.out(hidden_output)
--- train.py
+++ train.py
@@ -1,3 +1,166 @@
 from jaxtyping import install_import_hook
+import jax.numpy as jnp
+from flax import nnx
+from jaxtyping import Float, Array, Bool, Int
+import jax
+import optax
+import argparse
+import datasets as dats
+from torch.utils.data import DataLoader
+
+from model import DecoderModel
 
 hook = install_import_hook("TransformerDecoder", "beartype.beartype")
+
+
+@nnx.jit
+def train_batch(batch, model: DecoderModel, loss_fn, optimizer, pad_idx):
+    source: Int[Array, "B L1"] = batch["xq_context_padded"]
+    target_shifted: Int[Array, "B L2"] = batch["yq_sos_padded"]
+    target: Int[Array, "B L2"] = batch["yq_padded"]
+    full_input: Int[Array, "B L"] = jnp.concatenate([source, target_shifted], axis=1)
+    B, L = full_input.shape
+    _, L1 = source.shape
+
+    def calculate_loss(model):
+        causal_mask: Bool[Array, "B 1 L L"] = jnp.tril(
+            jnp.ones((B, 1, L, L), dtype=bool)
+        )
+        padding_mask: Bool[Array, "B 1 1 L"] = (full_input != pad_idx)[
+            :, jnp.newaxis, jnp.newaxis, :
+        ]
+        attention_mask: Bool[Array, "B 1 L L"] = causal_mask & padding_mask
+
+        model_logits: Float[Array, "B L V"] = model(
+            full_input, None, attention_mask, None
+        )
+        predicted_logits: Int[Array, "B L2 V"] = model_logits[:, L1 - 1 : -1, :]
+        target_loss: Float[Array, "B L2"] = loss_fn(predicted_logits, target)
+        loss_mask: Bool[Array, "B L2"] = target != pad_idx
+
+        return (target_loss * loss_mask) / jnp.sum(loss_mask), predicted_logits
+
+    (loss, logits), grads = nnx.value_and_grad(calculate_loss, has_aux=True)(model)
+    optimizer.update(model, grads)
+
+    accuracy = jnp.mean(jnp.argmax(logits, axis=-1) == target)
+    return loss, accuracy
+
+
+def val_batch(batch, model: DecoderModel, loss_fn, pad_idx):
+    source: Int[Array, "B L1"] = batch["xq_context_padded"]
+    target_shifted: Int[Array, "B L2"] = batch["yq_sos_padded"]
+    target: Int[Array, "B L2"] = batch["yq_padded"]
+    full_input: Int[Array, "B L"] = jnp.concatenate([source, target_shifted], axis=1)
+    B, L = full_input.shape
+    _, L1 = source.shape
+
+    causal_mask: Bool[Array, "B 1 L L"] = jnp.tril(jnp.ones((B, 1, L, L), dtype=bool))
+    padding_mask: Bool[Array, "B 1 1 L"] = (full_input != pad_idx)[
+        :, jnp.newaxis, jnp.newaxis, :
+    ]
+    attention_mask: Bool[Array, "B 1 L L"] = causal_mask & padding_mask
+
+    model_logits: Float[Array, "B L V"] = model(full_input, None, attention_mask, None)
+    predicted_logits: Int[Array, "B L2"] = model_logits[:, L1 - 1 : -1, :]
+    target_loss: Float[Array, "B L2"] = loss_fn(predicted_logits, target)
+    loss_mask: Bool[Array, "B L2"] = target != pad_idx
+
+    accuracy = jnp.mean(jnp.argmax(predicted_logits, axis=-1) == target)
+
+    return (target_loss * loss_mask) / jnp.sum(loss_mask), accuracy
+
+
+def validate(model, val_loader, loss_fn, pad_idx):
+    avg_loss, avg_acc = 0.0, 0.0
+    for batch in val_loader:
+        jax_batch = jax.tree_util.tree_map(
+            lambda x: jnp.array(x.numpy()) if hasattr(x, "numpy") else x, batch
+        )
+        loss, acc = val_batch(jax_batch, model, loss_fn, pad_idx)
+        avg_loss += loss
+        avg_acc += acc
+    return avg_loss / len(val_loader), avg_acc / len(val_loader)
+
+
+def train(
+    model, train_loader, val_loader, loss_fn, num_epochs, pad_idx, log_every, val_every
+):
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch}\n-------------------------------")
+        model.train()
+
+        for step, batch in enumerate(train_loader):
+            jax_batch = jax.tree_util.tree_map(
+                lambda x: jnp.array(x.numpy()) if hasattr(x, "numpy") else x, batch
+            )
+            loss, acc = train_batch(jax_batch, model, loss_fn, optimizer, pad_idx)
+            if step % log_every == 0:
+                print(f"Step {step} | Loss: {loss:.4f} | Acc: {acc:.4f}")
+            if step % val_every == 0:
+                val_loss, val_acc = validate(model, val_loader, loss_fn, pad_idx)
+                print(f"VALIDATION | Loss: {loss:.4f} | Acc: {acc:.4f}")
+
+    print("Training complete.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    hidden_size = args.hidden_size
+    interm_size = args.interm_size
+    num_heads = args.num_heads
+    vocab_size = args.vocab_size
+    seq_length = args.seq_length
+    dropout_prob = args.dropout_prob
+    nlayers = args.nlayers
+    learning_rate = args.learning_rate
+    weight_decay = args.weight_decay
+    num_epochs = args.num_epochs
+    batch_size = args.batch_size
+    log_every = args.log_every
+    val_every = args.val_every
+    episode_type = args.episode_type
+
+    D_train, D_val = dats.get_dataset(episode_type)
+    train_loader = DataLoader(
+        D_train,
+        batch_size=batch_size,
+        collate_fn=lambda x: dats.make_biml_batch(x, D_train.langs),
+        shuffle=True,
+    )
+    val_loader = DataLoader(
+        D_val,
+        batch_size=batch_size,
+        collate_fn=lambda x: dats.make_biml_batch(x, D_val.langs),
+        shuffle=False,
+    )
+    langs = D_train.langs
+    pad_idx = langs["output"].PAD_idx
+
+    rngs = nnx.Rngs(42)
+    model = DecoderModel(
+        hidden_size,
+        interm_size,
+        num_heads,
+        vocab_size,
+        seq_length,
+        nlayers,
+        dropout_prob,
+        rngs,
+    )
+    tx = optax.adamw(learning_rate=learning_rate, weight_decay=weight_decay)
+    optimizer = nnx.Optimizer(model, tx, wrt=nnx.Param)
+    loss_fn = optax.softmax_cross_entropy_with_integer_labels
+
+    train(
+        model,
+        train_loader,
+        val_loader,
+        loss_fn,
+        num_epochs,
+        pad_idx,
+        log_every,
+        val_every,
+    )
